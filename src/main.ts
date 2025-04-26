2a7d4beba6e2f2725ff5584af176d6d5b8c6205b
Jeff Phan <nostarwhere.pc@gmail.com>
2025-04-26 14:45:14 +0700

    add logger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('NestApplication');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
 bootstrap().catch((err) => console.log(err));
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -13,12 +13,17 @@ import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { Logger } from '@nestjs/common';
 @Controller('tasks')
 export class TasksController {
+  private logger = new Logger('TasksController');
   constructor(private tasksService: TasksService) {}
 
   @Get()
   getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    this.logger.verbose(
+      `Retrieving all tasks with filters: ${JSON.stringify(filterDto)}`,
+    );
     if (Object.keys(filterDto).length) {
       return this.tasksService.getTasksWithFilters(filterDto);
     }
@@ -27,16 +32,21 @@ export class TasksController {
 
   @Get('/:id')
   getTaskById(@Param('id') id: string): Task {
+    this.logger.verbose(`Retrieving task with id: ${id}`);
     return this.tasksService.getTaskById(id);
   }
 
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    this.logger.verbose(
+      `Creating a new task. Data: ${JSON.stringify(createTaskDto)}`,
+    );
     return this.tasksService.createTask(createTaskDto);
   }
 
   @Delete('/:id')
   deleteTask(@Param('id') id: string): void {
+    this.logger.verbose(`Deleting task with id: ${id}`);
     return this.tasksService.deleteTask(id);
   }
 
@@ -45,6 +55,9 @@ export class TasksController {
     @Param('id') id: string,
     @Body() updateTaskStatusDto: UpdateTaskStatusDto,
   ): Task {
+    this.logger.verbose(
+      `Updating task with id: ${id}. Data: ${JSON.stringify(updateTaskStatusDto)}`,
+    );
     const { status } = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
   }
